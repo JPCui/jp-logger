a975a223957ff7005e266191b78af1a0b2820142
JPCui <624498030@qq.com>
2017-03-28 11:51:23 +0800

    批量查询接口

--- src/main/java/cn/cjp/logger/util/IPUtil.java
+++ src/main/java/cn/cjp/logger/util/IPUtil.java
@@ -1,7 +1,15 @@
 package cn.cjp.logger.util;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -12,6 +20,11 @@ public class IPUtil {
 
 	private static final Logger LOGGER = Logger.getLogger(IPUtil.class);
 
+	/**
+	 * 
+	 * @param ip
+	 * @return province
+	 */
 	public static String getCityByIp(String ip) {
 		String api = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=" + ip;
 		Connection conn = Jsoup.connect(api).ignoreContentType(true).ignoreHttpErrors(true);
@@ -26,6 +39,67 @@ public class IPUtil {
 			LOGGER.warn(e.toString(), e);
 		}
 		return null;
+	}
+
+	public static Map<String, String> getCityByIps(String[] ips) {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug(String.format("Query ips(%d) info.", ips.length));
+		}
+
+		final ExecutorService service = Executors.newCachedThreadPool();
+		Map<String, Future<String>> futures = new HashMap<>();
+		for (String ip : ips) {
+			Future<String> future = service.submit(Task.newInstance(ip));
+			futures.put(ip, future);
+		}
+
+		Map<String, String> infoMap = new HashMap<>();
+		for (String ip : futures.keySet()) {
+			Future<String> future = futures.get(ip);
+			try {
+				String info = future.get(5, TimeUnit.SECONDS);
+				infoMap.put(ip, info);
+			} catch (InterruptedException | ExecutionException | TimeoutException e) {
+				LOGGER.error(e, e);
+			}
+		}
+		service.shutdown();
+		return infoMap;
+	}
+
+	public static void main(String[] args) {
+		String[] ips = { "202.196.35.35", "202.196.35.36", "202.196.35.37", "202.196.35.37", "101.200.143.36",
+				"101.201.143.36" };
+		Object r = getCityByIps(ips);
+		LOGGER.info(r);
+
+		// for (String ip : ips) {
+		// LOGGER.info(getCityByIp(ip));
+		// }
+	}
+
+}
+
+class Task implements Callable<String> {
+
+	String ip;
+
+	private Task() {
+	}
+
+	public static Task newInstance(String ip) {
+		Task task = new Task();
+		task.ip = ip;
+		return task;
+	}
+
+	@Override
+	public String call() throws Exception {
+		return IPUtil.getCityByIp(ip);
+	}
+
+	public String getIp() {
+		return ip;
 	}
 
 }
